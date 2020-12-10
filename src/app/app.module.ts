16963ecafad965065e2a416b0314c0fc65b29c4d
sadafmush <sadafmush99@gmail.com>
2020-12-10 22:41:11 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { appRoutingModule } from './app.routing';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AlertComponent } from './_components';
+import { AuditComponent } from './audit';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2OrderModule } from 'ng2-order-pipe';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule,
+        ReactiveFormsModule,
+        NgxPaginationModule,
+        Ng2OrderModule,
+        Ng2SearchPipeModule,
+        FormsModule
+ ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        AuditComponent,
+        AlertComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    
+    ],
+    bootstrap: [AppComponent]
+})
+
+
+
+export class AppModule{};
+
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,60 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+import { User } from '@/_models';
+import { UserService, AuthenticationService } from '@/_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    p:number=1;
+    users = [];
+    date:string;
+
+    time:string;
+    
+    public format:any;
+    today:any;
+    firstName:any;
+    constructor(
+        private authenticationService: AuthenticationService,
+        private userService: UserService
+    ) {
+        this.currentUser = this.authenticationService.currentUserValue;
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id)
+            .pipe(first())
+            .subscribe(() => this.loadAllUsers());
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll()
+            .pipe(first())
+            .subscribe(users => this.users = users);
+    }
+    search(){
+
+if(this.firstName==""){
+this.loadAllUsers();
+
+}
+else{
+    this.users=this.users.filter(res=>{
+        return res.firstName.toLocaleLowerCase().match(this.firstName.toLocaleLowerCase());
+    })
+}
+
+}
+
+key:string= 'id';
+reverse:boolean=false;
+sort(key){
+this.key=key;
+this.reverse= !this.reverse;
+}
+}
